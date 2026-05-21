6dc148a34f92fb22405a991d989684db637f655e
Amina Sultanova <aminasultanova007@gmail.com>
2026-05-21 13:29:23 +0600

    compatible versions

--- ai/knowledge_base.py
+++ ai/knowledge_base.py
@@ -1,8 +1,7 @@
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-
+from langchain_community.embeddings import HuggingFaceEmbeddings
 DATA_FOLDER = "data"
 CHROMA_DB_FOLDER = "./chroma_db"
 CHUNK_SIZE = 500
